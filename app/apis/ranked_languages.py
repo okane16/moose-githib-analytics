09fec12961673066cbcff04502d1bef949ceb425
Olivia Kane <olivia@fiveonefour.com>
2024-09-26 12:20:09 -0400

    remove cli log

--- app/apis/ranked_languages.py
+++ app/apis/ranked_languages.py
@@ -2,15 +2,14 @@
 # This file is where you can define your API templates for consuming your data
 # All query_params are passed in as strings, and are used within the sql tag to parameterize you queries
 
-from moose_lib import MooseClient, cli_log, CliLogData
+from moose_lib import MooseClient
+from typing import Optional
 
-
-def run(client: MooseClient, params, jwt_payload):
+def run(client: MooseClient, params, jwt_payload: Optional[dict] = None):
     rank_by = params.get("rank_by", ["total_projects"])[0]
     if rank_by not in ["total_projects", "total_repo_size_kb", "avg_repo_size_kb"]:
         raise ValueError("Invalid rank_by value. Must be one of: total_projects, total_repo_size_kb, avg_repo_size_kb")
     
-    cli_log(CliLogData(action="RankedLanguages", message=f"params: {params if params else 'no params'}", message_type="Info"))
     query = f'''
     SELECT 
         language, 
